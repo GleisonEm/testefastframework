4123e514e719545646d8af0a18509658b22eae1d
GleisonEm <gleison.spfc1208@hotmail.com>
2020-06-06 18:26:22 -0600

    migrations

---
+++ database/migrations/2020_06_06_235238_create_table_product.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableProduct extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('tittle');
+            $table->string('category');
+            $table->string('imagem', 1000);
+            $table->float('price');
+            $table->float('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
